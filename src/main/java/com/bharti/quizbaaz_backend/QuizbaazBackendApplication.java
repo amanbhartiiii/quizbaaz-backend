abd2165bc71d4666602dfaedeb37464a42d2a9a0
amanbhartiiii <amankumarbharti824112@gmail.com>
2025-09-19 14:31:33 +0530

    SpringBoot Project Setup

---
+++ src/main/java/com/bharti/quizbaaz_backend/QuizbaazBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bharti.quizbaaz_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizbaazBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizbaazBackendApplication.class, args);
+	}
+
+}
